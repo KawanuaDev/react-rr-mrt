548d9feca82d93aedd6030b8af31749512a805a1
Kawanua ID <46616237+kawanuaid@users.noreply.github.com>
2023-07-21 03:05:23 +0800

    Adding NavLink and Icons

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,20 +1,35 @@
 import * as React from 'react';
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+import { Box, NavLink } from '@mantine/core';
+import { IconHome, IconBorderAll, IconNews } from '@tabler/icons-react';
 
 export default function Navbar() {
+  const navigate = useNavigate();
   return (
     <nav>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/tabel">Tabel</Link>
-        </li>
-        <li>
-          <Link to="/posts">Posts</Link>
-        </li>
-      </ul>
+      <Box w={120}>
+        <NavLink
+          label="Home"
+          onClick={() => navigate('/')}
+          icon={<IconHome size="1.1rem" stroke={1.5} />}
+          variant="subtle"
+          active
+          />
+        <NavLink
+          label="Tabel"
+          onClick={() => navigate('/tabel')}
+          icon={<IconBorderAll size="1.1rem" stroke={1.5} />}
+          variant="light"
+          active
+          />
+        <NavLink
+          label="Posts"
+          onClick={() => navigate('/posts')}
+          icon={<IconNews size="1.1rem" stroke={1.5} />}
+          variant="filled"
+          active
+        />
+      </Box>
     </nav>
   );
 }
